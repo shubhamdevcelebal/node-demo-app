eff6ae97e13943e756bf74e18703afd505e62115
shubhamdevcelebal <shubham.tiwari@celebaltech.com>
2022-07-28 12:05:27 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let express=require("express")
+let app=express()
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+ })
+ 
+app.listen(3000,()=>{
+    console.log("Express app is listening on port")
+})
